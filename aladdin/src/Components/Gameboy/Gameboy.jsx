a799a50bffd1e499204457e5ac2acd45e8826415
Jhonas <jhowscts@gmail.com>
2024-10-07 01:53:56 -0300

    Reformulação completado front-end em React, tela de perguntas feita

--- aladdin/src/Components/Gameboy/Gameboy.jsx
+++ aladdin/src/Components/Gameboy/Gameboy.jsx
@@ -6,13 +6,25 @@ const Gameboy = () => {
     <div className="div">
         <div className='gameboy-div'>
             <div className="abuttom-div">
-                <button className="round-button"></button>
+                <button className="round-button">A</button>
             </div>
             <div className="tbuttom-div">
-                <button className="triangle-button" id="t1"></button>
+                <button className="triangle-button" id="t1">1</button>
                 <button className="triangle-button" id="t2">2</button>
                 <button className="triangle-button" id="t3">3</button>
                 <button className="triangle-button" id="t4">4</button>
+            </div>
+            <div className='rbuttom-div'>
+                <button className='retangle' id="return">return</button>
+                <button className='retangle' id="exit">exit</button>
+            </div>
+            <div className='tela'>
+                <h1>Pergunta</h1>
+                <button className="resposta" id="vm">vm</button>
+                <button className="resposta" id="am">am</button>
+                <button className="resposta" id="az">az</button>
+                <button className="resposta" id="vd">vd</button>
+                <spam id="numPergunta">25</spam>
             </div>
         </div>
     </div>
